1078ade0b1e45973bbcb3e285830b2b152d152e3
Akira Kawamata <autumn@piedey.co.jp>
2022-08-03 18:26:58 +0900

    add FirstConMysteryFixed2

---
+++ FirstConMysteryFixed2/FirstConMysteryFixed2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Text;
+
+System.Text.Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+File.WriteAllText("DON", "ドーン");
+File.WriteAllText("CON", "コーン", System.Text.Encoding.GetEncoding(GetACP()));
+
+[DllImport("kernel32.dll")][ResourceExposure(ResourceScope.None)] static extern int GetACP();
