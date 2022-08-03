ecec639517ebe6c14dbeac287d0b07abfed77e5f
Akira Kawamata <autumn@piedey.co.jp>
2022-08-03 20:08:02 +0900

    add ConstantPath

---
+++ ConstantPath/ConstantPath/Program.cs
@@ -0,0 +1,4 @@
+﻿const string fullpath1 = "C:\\Users\\(ユーザー名)\\Documents\\mysheet.xlsx";
+const string fullpath2 = @"C:\Users\(ユーザー名)\Documents\mysheet.xlsx";
+Console.WriteLine(fullpath1);
+Console.WriteLine(fullpath2);
