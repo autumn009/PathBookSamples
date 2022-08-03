6cf9544ce10eb9ca6c1a110d4f3c33da87047a9f
Akira Kawamata <autumn@piedey.co.jp>
2022-08-03 19:40:50 +0900

    add BadPathCreation

---
+++ BadPathCreation/BadPathCreation/Program.cs
@@ -0,0 +1,8 @@
+﻿var name = "mysheet.xlsx";
+var dir1 = "Documents";
+var dir2 = "(ユーザー名)";
+var dir3 = "Users";
+var drive = "C:";
+var separator = "\\";
+var path = drive + separator + dir3 + separator + dir2 + separator + dir1 + name;
+Console.WriteLine(path);
