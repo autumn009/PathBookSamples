e86cf009c8908949145eedc65926cc3806cb95fe
Akira Kawamata <autumn@piedey.co.jp>
2022-08-03 20:05:05 +0900

    fix separator

--- BadPathCreation/BadPathCreation/Program.cs
+++ BadPathCreation/BadPathCreation/Program.cs
@@ -4,5 +4,5 @@ var dir2 = "(ユーザー名)";
 var dir3 = "Users";
 var drive = "C:";
 var separator = "\\";
-var path = drive + separator + dir3 + separator + dir2 + separator + dir1 + name;
+var path = drive + separator + dir3 + separator + dir2 + separator + dir1 + separator + name;
 Console.WriteLine(path);
