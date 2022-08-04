1be166add8187815b32fe256f0890a1972b75ce2
Akira Kawamata <autumn@piedey.co.jp>
2022-08-04 11:37:22 +0900

    add CreateDirectory

---
+++ CreateDirectory/CreateDirectory/Program.cs
@@ -0,0 +1,18 @@
+﻿DumpDirectory();
+Console.WriteLine("CreateDirectory!");
+Directory.CreateDirectory("Sample");
+DumpDirectory();
+Console.WriteLine("CreateDirectory!");
+Directory.CreateDirectory("Sample");
+DumpDirectory();
+
+void DumpDirectory()
+{
+	foreach (var item in Directory.EnumerateDirectories("."))
+	{
+		Console.Write($"[{item.Replace(".\\","")}]");
+	}
+	Console.WriteLine();
+	Console.WriteLine($"found {Directory.EnumerateDirectories(".").Count()} directories");
+}
+
