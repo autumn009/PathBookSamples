d7ccc56ad784644c7f664c402340b3283c9d719e
Akira Kawamata <autumn@piedey.co.jp>
2022-08-04 18:31:24 +0900

    add DriveAndCurrentDirectory

---
+++ DriveAndCurrentDirectory/DriveAndCurrentDirectory/Program.cs
@@ -0,0 +1,16 @@
+﻿Directory.SetCurrentDirectory(@"m:\");
+Directory.CreateDirectory("Sample1");
+Directory.SetCurrentDirectory("Sample1");
+File.WriteAllText("mytext.txt","Hello!");
+Console.WriteLine($"Current Directory={Directory.GetCurrentDirectory()}");
+Console.WriteLine(File.ReadAllText("m:mytext.txt"));
+Directory.SetCurrentDirectory(@"n:\");
+Console.WriteLine($"Current Directory={Directory.GetCurrentDirectory()}");
+try
+{
+    Console.WriteLine(File.ReadAllText("m:mytext.txt"));
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine(e.Message);
+}
