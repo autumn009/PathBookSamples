1567721b56ebf04d59dfbc16b69431346d4c72ea
Akira Kawamata <autumn@piedey.co.jp>
2022-08-04 15:31:39 +0900

    add CreateFile

---
+++ CreateFile/CreateFile/Program.cs
@@ -0,0 +1,24 @@
+﻿// 必ず新規に作る
+using (var s1 = new FileStream("sample.bin", FileMode.CreateNew))
+{
+    s1.WriteByte(0x55);
+}
+dump();
+// 追加書き込み
+using (var s1 = new FileStream("sample.bin", FileMode.Append))
+{
+    s1.WriteByte(0xAA);
+}
+dump();
+// 上書き書き込み
+using (var s1 = new FileStream("sample.bin", FileMode.Create))
+{
+    s1.WriteByte(0x5A);
+}
+dump();
+
+void dump()
+{
+    var bytes = File.ReadAllBytes("sample.bin");
+    Console.WriteLine(String.Join(',', bytes.Select(c=>c.ToString("X2"))));
+}
