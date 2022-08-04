6772b11a099f4e215b96339b8e2eb9bc094a1243
Akira Kawamata <autumn@piedey.co.jp>
2022-08-04 17:05:32 +0900

    add ChangeDirectory

---
+++ ChangeDirectory/ChangeDirectory/Program.cs
@@ -0,0 +1,18 @@
+﻿// 開始時のカレントディレクトリ
+var oldCurrentDirectory = Directory.GetCurrentDirectory();
+// ディレクトリ作成
+var directoryInfo = Directory.CreateDirectory("Sample");
+// カレントディレクトリ変更
+Directory.SetCurrentDirectory("Sample");
+// myText.txtを作成
+File.WriteAllText("myText.txt","Hello!");
+// カレントディレクトリを出力
+Console.WriteLine($"Current Directory={Directory.GetCurrentDirectory()}");
+// カレントディレクトリからmyText.txtを主強く
+Console.WriteLine(File.ReadAllText("myText.txt"));
+// カレントディレクトリを戻す
+Directory.SetCurrentDirectory(oldCurrentDirectory);
+// 作ったディレクトリの削除
+Directory.Delete(directoryInfo.FullName,true);
+// カレントディレクトリを出力
+Console.WriteLine($"Current Directory={Directory.GetCurrentDirectory()}");
