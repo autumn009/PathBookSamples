d27c45d521f7046f1cf10f065ca4671ce2c5fb59
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 15:30:00 +0900

    add DoubleDotDirectory

---
+++ DoubleDotDirectory/DoubleDotDirectory/Program.cs
@@ -0,0 +1,13 @@
+﻿Directory.SetCurrentDirectory(@"c:\Sample");
+var di = new DirectoryInfo(".");
+
+Console.WriteLine("The Directory Contains:");
+foreach (var item in di.EnumerateDirectories())
+{
+    Console.WriteLine(item.Name);
+}
+Console.WriteLine("That's all!");
+sub(".");
+sub("..");
+
+void sub(string name) => Console.WriteLine($"{name} exists={Directory.Exists(name)}");
