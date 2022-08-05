dcaf4c8009ab40ad4a326e57e5d7088268aa1e93
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 18:52:13 +0900

    add DirectoryBase

---
+++ DirectoryBase/DirectoryBase/Program.cs
@@ -0,0 +1,11 @@
+﻿Directory.SetCurrentDirectory(@"c:\Sample");
+sub(".");
+sub(@"c:\Sample");
+Directory.SetCurrentDirectory(@"c:\");
+sub(@"Sample");
+
+void sub(string baseDirectoery)
+{
+	Console.WriteLine($"base is {baseDirectoery}");
+	Console.WriteLine(String.Join(',', Directory.EnumerateFiles(baseDirectoery, "*.txt", SearchOption.AllDirectories)));
+}
