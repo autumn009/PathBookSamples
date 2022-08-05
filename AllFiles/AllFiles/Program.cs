e0480a76476310ece6cb4e2040c4c7f86a478f0b
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 16:52:38 +0900

    add AllFiles

---
+++ AllFiles/AllFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("by GetFiles");
+DateTime start = DateTime.Now;
+var fles = Directory.GetFiles(@"n:\tools","*.*", SearchOption.AllDirectories);
+Console.WriteLine(DateTime.Now-start);
+
+Console.WriteLine("by EnumerateFiles");
+DateTime start2 = DateTime.Now;
+var fles2 = Directory.EnumerateFiles(@"n:\tools", "*.*", SearchOption.AllDirectories);
+Console.WriteLine(DateTime.Now - start2);
+
+
+
