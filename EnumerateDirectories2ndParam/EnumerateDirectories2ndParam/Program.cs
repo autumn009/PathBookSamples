54b7200cf04748a50a1e37e2f7a2f72051a83d05
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 20:15:52 +0900

    add EnumerateDirectories2ndParam

---
+++ EnumerateDirectories2ndParam/EnumerateDirectories2ndParam/Program.cs
@@ -0,0 +1,15 @@
+﻿Directory.CreateDirectory(@"c:\Sample\a\b.b\c.c.c\d.d.d.d\e.e.e.e.e");
+Directory.SetCurrentDirectory(@"c:\Sample");
+
+sub("*");
+sub("*.*");
+sub("*.*.*");
+
+void sub(string searchPattern)
+{
+    Console.WriteLine($"searchPattern={searchPattern}");
+    foreach (var item in Directory.GetDirectories(".", searchPattern, SearchOption.AllDirectories))
+    {
+        Console.WriteLine(item);
+    }
+}
