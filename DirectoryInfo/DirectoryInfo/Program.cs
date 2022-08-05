c491e80ad2ade97bb65b64b0078434945119f449
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 19:49:25 +0900

    add DirectoryInfo

---
+++ DirectoryInfo/DirectoryInfo/Program.cs
@@ -0,0 +1,17 @@
+﻿Directory.SetCurrentDirectory(@"c:\Sample");
+
+var di = new DirectoryInfo(".");
+
+foreach (FileInfo item in di.EnumerateFiles("*.*", SearchOption.AllDirectories))
+{
+    Console.WriteLine($"Name={item.Name}");
+    Console.WriteLine($"FullName={item.FullName}");
+}
+
+foreach (DirectoryInfo item in di.EnumerateDirectories("*.*", SearchOption.AllDirectories))
+{
+    Console.WriteLine($"Name={item.Name}");
+    Console.WriteLine($"FullName={item.FullName}");
+}
+
+
