d1fecfc7976b1cd7c314dc26ffb36a1ad71bc7cc
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 13:09:33 +0900

    add ToAbsolutePath

---
+++ ToAbsolutePath/ToAbsolutePath/Program.cs
@@ -0,0 +1,14 @@
+﻿Directory.SetCurrentDirectory(@"c:\Sample");
+string[] paths = {
+    @"Loid\codename.txt",
+    @"c:\Sample\Loid\codename.txt",
+    @"..\sample\Loid\codename.txt",
+    @"Yor\..\Loid\codename.txt",
+    @"c:\Sample\.\Anya\..\Yor\..\.\Loid\codename.txt"
+};
+foreach (var path in paths)
+{
+    Console.WriteLine($"from {path}");
+    var a = Path.GetFullPath(path);
+    Console.WriteLine($"converted {a}");
+}
