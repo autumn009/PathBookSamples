ab9c81682756efc7b9ea368a6332a1e81981ce29
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 14:12:30 +0900

    add exists

---
+++ Exists/Exists/Program.cs
@@ -0,0 +1,14 @@
+﻿Directory.CreateDirectory("mydir");
+File.WriteAllText("myfile.txt","Hello!");
+
+sub("mydir");
+sub("myfile.txt");
+sub("nofile.txt");
+
+void sub(string path)
+{
+    Console.WriteLine($"about {path}");
+    Console.WriteLine($"Directory.Exists={Directory.Exists(path)}");
+    Console.WriteLine($"File.Exists={File.Exists(path)}");
+}
+
