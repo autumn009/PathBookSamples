57e31cd18a8bab512d9dcaa2a294246629a5a6de
Akira Kawamata <autumn@piedey.co.jp>
2022-08-05 22:20:21 +0900

    add EnumerateFiles2ndParam

---
+++ EnumerateFiles2ndParam/EnumerateFiles2ndParam/Program.cs
@@ -0,0 +1,19 @@
+﻿Directory.CreateDirectory(@"c:\Sample");
+Directory.SetCurrentDirectory(@"c:\Sample");
+File.WriteAllText("t", "sample");
+File.WriteAllText("t.t", "sample");
+File.WriteAllText("t.t.t", "sample");
+File.WriteAllText("t.t.t.t", "sample");
+
+sub("*");
+sub("*.*");
+sub("*.*.*");
+
+void sub(string searchPattern)
+{
+    Console.WriteLine($"searchPattern={searchPattern}");
+    foreach (var item in Directory.EnumerateFiles(".", searchPattern, SearchOption.AllDirectories))
+    {
+        Console.WriteLine(item);
+    }
+}
