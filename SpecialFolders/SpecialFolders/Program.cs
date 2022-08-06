0608c719c7dd9b5a884d0767f85980a475169faa
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 10:18:24 +0900

    add SpecialFolders

---
+++ SpecialFolders/SpecialFolders/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyComputer));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
