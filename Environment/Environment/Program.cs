af77b33ffd8cd152e2ee5e426a0eb9004679bc51
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 10:58:16 +0900

    add Environment

---
+++ Environment/Environment/Program.cs
@@ -0,0 +1,6 @@
+﻿// 環境変数に書き込む
+Environment.SetEnvironmentVariable("Sample",@"c:\Sample");
+// 環境変数から読み込む
+Console.WriteLine(Environment.GetEnvironmentVariable("Sample") ?? "(null)");
+Console.WriteLine(Environment.GetEnvironmentVariable("Sample", EnvironmentVariableTarget.User)??"(null)");
+Console.WriteLine(Environment.GetEnvironmentVariable("Sample", EnvironmentVariableTarget.Machine) ?? "(null)");
