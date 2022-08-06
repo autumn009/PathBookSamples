aadc8233d0b6936f9caaea6017c6b66814ccf398
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 16:50:50 +0900

    add PathCombine

---
+++ PathCombine/PathCombine/Program.cs
@@ -0,0 +1,11 @@
+﻿var d = @"c:";
+var a = "a";
+var b = "b";
+var c = "c.txt";
+
+// bad case
+Console.WriteLine(d + "\\" + a + "\\" + b + "\\" + c);
+// better case
+Console.WriteLine(d + Path.DirectorySeparatorChar + a + Path.DirectorySeparatorChar + b + Path.DirectorySeparatorChar + c);
+// more better case
+Console.WriteLine(Path.Combine(d, a, b, c));
