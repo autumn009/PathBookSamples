cd81753f059f41c9017719aea5c7ce8d3d24afd6
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 17:01:52 +0900

    add PathCombineLimit

---
+++ PathCombineLimit/PathCombineLimit/Program.cs
@@ -0,0 +1,5 @@
+﻿var r = Path.Combine("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+Console.WriteLine(r);
+var array = Enumerable.Range(0, 26).Select(c => ((char)(c + 'a')).ToString()).ToArray();
+var r2 = Path.Combine(array);
+Console.WriteLine(r2);
