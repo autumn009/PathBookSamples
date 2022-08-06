37a4b38d7f4379a25ca772126d1cade7d2fddcdd
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 14:52:50 +0900

    add SymbolicLink

---
+++ SymbolicLink/SymbolicLink/Program.cs
@@ -0,0 +1,4 @@
+﻿Directory.CreateDirectory(@"c:\Sample\real");
+Directory.CreateSymbolicLink(@"c:\Sample\refer", @"c:\Sample\real");
+File.WriteAllText(@"c:\Sample\real\mytext.txt","Hello!");
+Console.WriteLine(File.ReadAllText(@"c:\Sample\refer\mytext.txt"));
