b93c4b870512acdd51dd87448705ead7bc4ec60e
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 16:21:28 +0900

    add AddRemoveChangeExt

---
+++ AddRemoveChangeExt/AddRemoveChangeExt/Program.cs
@@ -0,0 +1,11 @@
+﻿var path1 = @"dir\myfile.txt";
+var path2 = @"dir\myfile";
+Console.WriteLine($"add ext '.txt' to {path2}={Path.ChangeExtension(path2, "txt")}");
+Console.WriteLine($"remove ext from {path1}={Path.GetFileNameWithoutExtension(path1)}");
+Console.WriteLine($"remove ext from {path1}={Path.ChangeExtension(path1, null)}");
+Console.WriteLine($"remove ext from {path1}={Path.ChangeExtension(path1, "")}");
+Console.WriteLine($"change ext to '.bak' {path1}={Path.ChangeExtension(path1, "bak")}");
+
+
+
+
---
+++ Root/Root/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine(Path.GetPathRoot(@"c:\a\b\c\d\e.txt"));
+Console.WriteLine(Path.GetPathRoot(@"x.txt"));
+Console.WriteLine(Path.GetPathRoot(@"\\server\share\dir\filename.txt"));
+Console.WriteLine(Path.GetPathRoot(@"\\.\c:\a\b\c\d\e.txt"));
