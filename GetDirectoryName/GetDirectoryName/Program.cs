6f83054531a5dea7e5ada59f6d67781920696fc4
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 15:24:14 +0900

    add GetDirectoryName

---
+++ GetDirectoryName/GetDirectoryName/Program.cs
@@ -0,0 +1,5 @@
+﻿var path = @"c:\a\b\c\d\e.txt";
+
+Console.WriteLine($"Path={path}");
+Console.WriteLine($"FileName={Path.GetFileName(path)}");
+Console.WriteLine($"DirectoryName={Path.GetDirectoryName(path)}");
