b843bff48c5adb7e31746cc03ba9b96b0acdfe57
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 15:30:23 +0900

    add GetDirectoryName2

---
+++ GetDirectoryName2/GetDirectoryName2/Program.cs
@@ -0,0 +1,13 @@
+﻿var path = @"c:\a\b\c\d\e.txt";
+var s = path;
+for (; ; )
+{
+    var name = Path.GetFileName(s);
+    if (name == "")
+    {
+        Console.WriteLine($"[{s}]");
+        break;
+    }
+    Console.WriteLine($"[{name}]");
+    s = Path.GetDirectoryName(s);
+}
