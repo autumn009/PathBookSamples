35b43728939e0e7e6a1034f494c6a1c29a31539a
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 14:12:06 +0900

    add unc

---
+++ UNC/UNC/Program.cs
@@ -0,0 +1,10 @@
+﻿var unc = @"\\akane4\Sample";
+var di = Directory.CreateDirectory(Path.Combine(unc,"MyDir"));
+Directory.SetCurrentDirectory(di.FullName);
+Console.WriteLine($"CurrentDirectory={Directory.GetCurrentDirectory()}");
+File.WriteAllText("hello.txt","Hello");
+File.WriteAllText("world.txt", "World");
+foreach (var item in di.EnumerateFiles())
+{
+    Console.WriteLine(item.FullName);
+}
