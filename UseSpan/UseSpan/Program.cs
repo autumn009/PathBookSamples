1aaf2fdfb9f12ac050c18818e9cf0c76cd7a158a
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 20:07:01 +0900

    add UseSpan

---
+++ UseSpan/UseSpan/Program.cs
@@ -0,0 +1,10 @@
+﻿var path = @"c:\Sample\myfile.txt";
+
+Console.WriteLine("by string");
+string r = Path.GetFileName(path);
+Console.WriteLine(r);
+
+Console.WriteLine("by ReadOnlySpan<char>");
+ReadOnlySpan<char> r2 = Path.GetFileName(path.AsSpan());
+foreach (var item in r2) Console.Write(item);
+Console.WriteLine();
