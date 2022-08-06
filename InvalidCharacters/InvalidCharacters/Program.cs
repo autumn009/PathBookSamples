87dbf454968e1de4d9ce20e96bbc4fe654800ffe
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 18:59:59 +0900

    add InvalidCharacters

---
+++ InvalidCharacters/InvalidCharacters/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("InvalidPathChars");
+Console.WriteLine(String.Join(',', Path.GetInvalidPathChars().Select(ch => ((int)ch).ToString("X04"))));
+Console.WriteLine("InvalidFileNameChars");
+Console.WriteLine(String.Join(',', Path.GetInvalidFileNameChars().Select(ch => ((int)ch).ToString("X04"))));
