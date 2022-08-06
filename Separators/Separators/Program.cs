38a103cfbc941d74cd203ca8bd7b8add1c1f5612
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 18:46:03 +0900

    add Separators

---
+++ Separators/Separators/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine($"Path.PathSeparator={Path.PathSeparator}");
+Console.WriteLine($"Path.DirectorySeparatorChar={Path.DirectorySeparatorChar}");
+Console.WriteLine($"Path.AltDirectorySeparatorChar={Path.AltDirectorySeparatorChar}");
+Console.WriteLine($"Path.VolumeSeparatorChar={Path.VolumeSeparatorChar}");
