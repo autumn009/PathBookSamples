627c61a2f97777740869acd0a5786c5f48fac5e1
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 12:54:34 +0900

    add TempFile

---
+++ TempFile/TempFile/Program.cs
@@ -0,0 +1,8 @@
+﻿var tempPath = Path.GetTempPath();
+var tempFileName = Path.GetTempFileName();
+var tempRandomoFileName = Path.GetRandomFileName();
+Console.WriteLine($"tempPath={tempPath}");
+Console.WriteLine($"tempFileName={tempFileName}");
+Console.WriteLine($"tempFileName exists={File.Exists(tempFileName)}");
+Console.WriteLine($"tempRandomoFileName={tempRandomoFileName}");
+File.Delete(tempFileName);
