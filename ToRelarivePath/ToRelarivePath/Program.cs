7f9c4f14771d4cfd843887056b154a1964011f97
Akira Kawamata <autumn@piedey.co.jp>
2022-08-06 17:13:00 +0900

    add ToRelarivePath

---
+++ ToRelarivePath/ToRelarivePath/Program.cs
@@ -0,0 +1,4 @@
+﻿var relavePath = Path.GetRelativePath(@"c:\dir1", @"c:\dir1\dir2\myFile.txt");
+Console.WriteLine(relavePath);
+var relavePath2 = Path.GetRelativePath(@"c:\dir1", @"c:\dir1\..\dir1\dir2\.\myFile.txt");
+Console.WriteLine(relavePath2);
